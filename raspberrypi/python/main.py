9c405096055344edae9865c354de71cb8cddde88
lukehaas <lukehaas@users.noreply.github.com>
2018-04-14 13:38:01 +0100

    remove demo image code

--- raspberrypi/python/main.py
+++ raspberrypi/python/main.py
@@ -26,9 +26,9 @@
 
 import epd7in5
 import Image
-import ImageDraw
-import ImageFont
-#import imagedata
+# import ImageDraw
+# import ImageFont
+# import imagedata
 
 EPD_WIDTH = 640
 EPD_HEIGHT = 384
@@ -38,16 +38,16 @@ def main():
     epd.init()
 
     # For simplicity, the arguments are explicit numerical coordinates
-    image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 1)    # 1: clear the frame
-    draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 24)
-    draw.rectangle((0, 6, 640, 30), fill = 0)
-    draw.text((200, 10), 'e-Paper demo', font = font, fill = 255)
-    draw.rectangle((200, 80, 600, 280), fill = 0)
-    draw.arc((240, 120, 580, 220), 0, 360, fill = 255)
-    draw.rectangle((0, 80, 160, 280), fill = 255)
-    draw.arc((40, 80, 180, 220), 0, 360, fill = 0)
-    epd.display_frame(epd.get_frame_buffer(image))
+    # image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 1)    # 1: clear the frame
+    # draw = ImageDraw.Draw(image)
+    # font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 24)
+    # draw.rectangle((0, 6, 640, 30), fill = 0)
+    # draw.text((200, 10), 'e-Paper demo', font = font, fill = 255)
+    # draw.rectangle((200, 80, 600, 280), fill = 0)
+    # draw.arc((240, 120, 580, 220), 0, 360, fill = 255)
+    # draw.rectangle((0, 80, 160, 280), fill = 255)
+    # draw.arc((40, 80, 180, 220), 0, 360, fill = 0)
+    # epd.display_frame(epd.get_frame_buffer(image))
 
     image = Image.open('monocolor.bmp')
     epd.display_frame(epd.get_frame_buffer(image))
